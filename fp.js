ebf6503a3282def99d69118683a009aea130056c
sk <sk@local>
2017-01-14 21:46:51 +0900

    Fix parse and error handling

--- fp.js
+++ fp.js
@@ -4,30 +4,48 @@ var FeedParser = require('feedparser')
 var request = require('request')
 var feed = process.argv[2]
 
-var req = request(feed)
+var req = request.get({
+  method: 'GET',
+  url: feed,
+  gzip: true,
+  headers: {
+    'User-Agent': 'request'
+  }
+})
+
 var feedparser = new FeedParser({})
 
 var items = []
 var meta = {}
 
+req.on('error', function (error) {
+  console.log(`Request error:${error}`)
+})
+
 req.on('response', function (res) {
-  req.pipe(feedparser)
-});
+  console.log(req)
+  if (res.statusCode !== 200) {
+    this.emit('error', new Error(`Bad status code ${res.statusCode}`))
+  }
+  else {
+    req.pipe(feedparser)
+  }
+})
 
 feedparser.on('meta',  (m) => {
   meta = m
-});
+})
 
 feedparser.on('readable', function () {
   var item
   while(item = this.read()) {
     items.push(item)
   }
-});
+})
 
 feedparser.on('end', () => {
   console.log(JSON.stringify({
     feed : meta,
     entries : items
   }))
-});
+})
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-require('dotenv').config();
+require('dotenv').config()
 
 var FeedParser = require('feedparser')
 var request = require('request')
@@ -14,21 +14,49 @@ app.use(compression())
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.get('/versions', (req, res) => {
+  res.json(process.versions)
+})
+
 app.get('/ajax/services/feed/load', function(req, res) {
   var feed = req.query.q
-  var feed_req = request(feed)
+  console.log(feed)
+  var feed_req = request({
+    method: 'GET',
+    url: feed,
+    gzip: true,
+    headers: {
+      'User-Agent': 'request'
+    }
+  })
   var feedparser = new FeedParser({})
 
   var items = []
   var meta = {}
 
+  feed_req.on('error', function (error) {
+    console.log(`Request error:${error}`)
+    res.status(502).send('Proxy error');
+//    res.status(502);
+//    res.render('error', { error: error });
+  })
+
   feed_req.on('response', function (res) {
-    feed_req.pipe(feedparser)
-  });
+    if (res.statusCode !== 200) {
+      this.emit('error', new Error(`Bad status code ${res.statusCode}`))
+    }
+    else {
+      feed_req.pipe(feedparser)
+    }
+  })
+
+  feedparser.on('error', function (error) {
+    console.log(error)
+  })
 
   feedparser.on('meta',  (m) => {
     meta = m
-  });
+  })
 
   feedparser.on('readable', function () {
     var item
@@ -37,7 +65,7 @@ app.get('/ajax/services/feed/load', function(req, res) {
       item.content = item.description
       items.push(item)
     }
-  });
+  })
 
   feedparser.on('end', () => {
     res.json({
@@ -48,8 +76,7 @@ app.get('/ajax/services/feed/load', function(req, res) {
         }
       }
     })
-  });
-
+  })
 })
 
 app.listen(app.get('port'), function() {
